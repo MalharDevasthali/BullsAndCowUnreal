2b087c68ec25831e32f0a66341514247eec1fd52
Malhar Devasthali <malhar@deftouch.co>
2021-03-11 13:31:53 +0530

    #Added Functions setup

--- BullCowGame-starter-kit 4.26/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit 4.26/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,11 +4,52 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Welcome to Bull and Cow Game");
-    PrintLine("Press Enter to Continue:");
+    PrintLine(TEXT("Welcome to Bull and Cow Game"));
+    PrintLine(TEXT("Press Enter to Continue:"));
+    InitGame();
 }
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
     ClearScreen();
+    if (hiddenIsogram == Input)
+    {
+        PrintLine(TEXT("Great You Won!"));
+        GameWon();
+    }
+    else
+    {
+        // for (int i = 0; i < Input.)
+        PrintLine(TEXT("Lost!"));
+
+        lives--;
+        if (lives > 0)
+        {
+            ContinueNextTry();
+        }
+        else
+        {
+            GameLost();
+        }
+    }
+}
+void UBullCowCartridge::InitGame()
+{
+    hiddenIsogram = TEXT("unreal");
+    lives = 4;
+}
+void UBullCowCartridge::ContinueNextTry()
+{
+}
+void UBullCowCartridge::QuitGame()
+{
+}
+void UBullCowCartridge::ReplayGame()
+{
+}
+void UBullCowCartridge::GameLost()
+{
+}
+void UBullCowCartridge::GameWon()
+{
 }
--- BullCowGame-starter-kit 4.26/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame-starter-kit 4.26/Source/BullCowGame/BullCowCartridge.h
@@ -14,7 +14,15 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString &Input) override;
+	void InitGame();
+	void ContinueNextTry();
+	void ReplayGame();
+	void GameLost();
+	void GameWon();
+	void QuitGame();
 
 	// Your declarations go below!
 private:
+	FString hiddenIsogram; //Isogram is a string where no words will appear more than once in a string
+	int32 lives;
 };
